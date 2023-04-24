bb1aee948eafb3a48a054d5862aa089a8fba1623
kareem hamed <khidmt55@gmail.com>
2023-04-24 04:20:35 +0200

    curriculum models

---
+++ app/Models/curriculum_file.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class curriculum_file extends Model
+{
+    use HasFactory;
+    protected $guarded=[];
+}
