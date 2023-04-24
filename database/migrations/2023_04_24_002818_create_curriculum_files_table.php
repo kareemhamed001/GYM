22b73ed6bf2d55fbfe6ded0b00691ca609a2fa56
kareem hamed <khidmt55@gmail.com>
2023-04-24 04:17:44 +0200

    add curriculum tables

---
+++ database/migrations/2023_04_24_002818_create_curriculum_files_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('curriculum_files', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',200)->nullable();
+            $table->text('description')->nullable();
+            $table->string('path')->nullable();
+            $table->tinyInteger('type')->comment('0->video,1->file');
+            $table->unsignedBigInteger('curriculum_id');
+            $table->foreign('curriculum_id')->references('id')->on('curricula')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('curriculum_files');
+    }
+};
