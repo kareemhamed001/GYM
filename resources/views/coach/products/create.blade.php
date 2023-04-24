5f610c9941135309a41988aa76ec47646affd181
kareem hamed <khidmt55@gmail.com>
2023-04-24 04:20:21 +0200

    remove previewImage function and move it to layout

--- resources/views/coach/products/create.blade.php
+++ resources/views/coach/products/create.blade.php
@@ -128,19 +128,6 @@
             }
         });
 
-        function previewImage(event) {
-            const file = event.target.files[0];
 
-            if (file) {
-                const reader = new FileReader();
-
-                reader.addEventListener("load", function() {
-                    const preview = document.getElementById("preview");
-                    preview.src = reader.result;
-                });
-
-                reader.readAsDataURL(file);
-            }
-        }
     </script>
 @endsection
