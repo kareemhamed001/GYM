2435a1b4f12c0fcc9548fa5efd1ee37ab3b8c66c
kareem hamed <khidmt55@gmail.com>
2023-04-26 01:16:50 +0200

    add edit user button

--- resources/views/coach/users/index.blade.php
+++ resources/views/coach/users/index.blade.php
@@ -168,8 +168,7 @@
                                         <circle cx="12" cy="12" r="3"></circle>
                                     </svg>
                                 </a>
-                                <a onclick="prepareusertoedit({{$user->id}})"
-                                   href="javascript:void(0);" class="action-btn btn-edit bs-tooltip me-2"
+                                <a href="{{route('users.edit',$user->id)}}" class="action-btn btn-edit bs-tooltip me-2"
                                    data-placement="top" title="Edit">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                          fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
