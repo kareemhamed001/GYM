18623807d7fc034ed90c68389f85281d5360758d
kareem hamed <khidmt55@gmail.com>
2023-04-27 19:44:33 +0200

    change seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          \App\Models\User::factory(10)->create();
-         \App\Models\coach::factory(10)->create();
+//         \App\Models\coach::factory(10)->create();
          \App\Models\course::factory(10)->create();
          \App\Models\brand::factory(10)->create();
          \App\Models\supplement::factory(10)->create();
