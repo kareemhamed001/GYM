85559a48feaaaec1c489863d408f9e8d81ac840e
kareem hamed <khidmt55@gmail.com>
2023-04-27 19:43:17 +0200

    add purchaseFactory

---
+++ database/factories/purchaseFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\supplement;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\purchase>
+ */
+class purchaseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $supplement=supplement::inRandomOrder()->first();
+        return [
+            'user_id' => User::inRandomOrder()->first()->id,
+            'supplement_id' => $supplement->id,
+            'number' => fake()->numberBetween(1,$supplement->quantity),
+            'price' => $supplement->price * ($supplement->discount/100),
+            'discount' => $supplement->discount,
+        ];
+    }
+}
