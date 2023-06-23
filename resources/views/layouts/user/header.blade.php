bf8cad1ea110b04632c23ee04be8d5da9375dabc
kareem hamed <khidmt55@gmail.com>
2023-06-23 21:10:25 +0300

    add login check

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -34,80 +34,87 @@
 
 
 
-                <li class="nav-item dropdown">
-                    <a href="javascript:void(0);" class="dropdown-toggle " type="button" id="dropdownMenuButton"
-                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <div class="avatar-container">
-                            <div class="avatar avatar-sm avatar-indicators avatar-online">
-                                <img alt="avatar"
-                                     src="{{asset(Auth::user()?->profile_image??'assets/images/logo/xlogo.png')}}"
-                                     class="rounded-circle">
+                @if(Auth::check() )
+                    <li class="nav-item dropdown">
+                        <a href="javascript:void(0);" class="dropdown-toggle " type="button" id="dropdownMenuButton"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div class="avatar-container">
+                                <div class="avatar avatar-sm avatar-indicators avatar-online">
+                                    <img alt="avatar"
+                                         src="{{asset(Auth::user()?->profile_image??'assets/images/logo/xlogo.png')}}"
+                                         class="rounded-circle">
+                                </div>
                             </div>
-                        </div>
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-
-
-                        <a class="dropdown-item " href="javascript:void(0)">
-
-                            <h6 class="text-break">{{Auth::user()?->name??'Name'}}</h6>
-                            <p class="text-muted">@if(Auth::check()&&Auth::user()->role_as==1)
-                                    Coach
-                                @else
-                                    User
-                                @endif</p>
-
                         </a>
 
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 
-                        @if(Auth::check()&&Auth::user()->role_as==1)
-                            <a class="dropdown-item d-flex align-items-center" href="{{url('/coach')}}">
 
-                                <i class="fa-light fa-dashboard"></i>
-                                <span>Dashboard</span>
+                            <a class="dropdown-item " href="javascript:void(0)">
+
+                                <h6 class="text-break">{{Auth::user()?->name??'Name'}}</h6>
+                                <p class="text-muted">@if(Auth::check()&&Auth::user()->role_as==1)
+                                        Coach
+                                    @else
+                                        User
+                                    @endif</p>
 
                             </a>
-                        @endif
 
 
-                        <a class="dropdown-item d-flex align-items-center" href="{{url('user/cart')}}">
-                            <i class="fa-light fa-shopping-cart"></i>
-                            <span>Cart</span>
-                        </a>
+                            @if(Auth::check()&&Auth::user()->role_as==1)
+                                <a class="dropdown-item d-flex align-items-center" href="{{url('/coach')}}">
 
-                        <a class="dropdown-item d-flex align-items-center" href="{{url('user/profile')}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round" class="feather feather-user">
-                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                <circle cx="12" cy="7" r="4"></circle>
-                            </svg>
-                            <span>Profile</span>
-                        </a>
+                                    <i class="fa-light fa-dashboard"></i>
+                                    <span>Dashboard</span>
+
+                                </a>
+                            @endif
 
 
+                            <a class="dropdown-item d-flex align-items-center" href="{{url('user/cart')}}">
+                                <i class="fa-light fa-shopping-cart"></i>
+                                <span>Cart</span>
+                            </a>
 
-                        <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}"
-                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round" class="feather feather-log-out">
-                                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-                                <polyline points="16 17 21 12 16 7"></polyline>
-                                <line x1="21" y1="12" x2="9" y2="12"></line>
-                            </svg>
-                            Logout
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
+                            <a class="dropdown-item d-flex align-items-center" href="{{url('user/profile')}}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                     stroke-linejoin="round" class="feather feather-user">
+                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                                <span>Profile</span>
+                            </a>
 
 
-                    </div>
 
-                </li>
+                            <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                     stroke-linejoin="round" class="feather feather-log-out">
+                                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                                    <polyline points="16 17 21 12 16 7"></polyline>
+                                    <line x1="21" y1="12" x2="9" y2="12"></line>
+                                </svg>
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+
+
+                        </div>
+
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a href="{{route('login')}}" class="btn  btn-primary">Login</a>
+                    </li>
+                @endif
+
 
                 <li class="nav-item theme-toggle-item ">
                     <a href="javascript:void(0);" class="nav-link text-black fw-bold theme-toggle">
